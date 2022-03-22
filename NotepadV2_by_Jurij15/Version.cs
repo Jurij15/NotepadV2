f75f9edddfe160952ef64c63c2b69787f5b3b935
Jurij15 <jurij.grom1@gmail.com>
2022-03-22 17:52:52 +0100

    some project changes

--- NotepadV2_by_Jurij15/Version.cs
+++ NotepadV2_by_Jurij15/Version.cs
@@ -8,8 +8,8 @@ namespace NotepadV2_by_Jurij15
 {
     public class Version
     {
-        public string VersionString = "0.4 alpha";
+        public string VersionString = "0.5 alpha";
 
-        public string PatchNotesVer = "0.4";
+        public string PatchNotesVer = "0.5";
     }
 }
