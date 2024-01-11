51f9546d44d07f48ac216fb84ce96eae0f7b70fa
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2024-01-11 16:19:46 +0100

    idk what happened

--- SimplePad/Dialogs/FontDialog.xaml.cs
+++ SimplePad/Dialogs/FontDialog.xaml.cs
@@ -5,8 +5,6 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using SimplePad.Pages;
-using SimplePad.Services;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,34 +12,20 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace SimplePad
+namespace SimplePad.Dialogs
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : WinUIEx.WindowEx
+    public sealed partial class FontDialog : Page
     {
-        public MainWindow()
+        public FontDialog()
         {
             this.InitializeComponent();
-
-            this.ExtendsContentIntoTitleBar = true;
-            this.SetTitleBar(AppTitleBar);
-
-            this.SetWindowSize(612, 750);
-
-            this.CenterOnScreen();
-        }
-
-        private void RootFrame_Loaded(object sender, RoutedEventArgs e)
-        {
-            RootFrame.Navigate(typeof(EditorPage));
-            DialogService.XamlRoot = RootGrid.XamlRoot;
         }
     }
 }
